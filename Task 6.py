83020654aaed9f779f70ef5163508f86d9403727
Mukhtar Mammadov <161587396+muxtarofficiall@users.noreply.github.com>
2024-05-05 21:22:00 +0400

    Create Task 6.py

---
+++ Task 6.py
@@ -0,0 +1,3 @@
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+first_names = [name.split()[0].lower() for name in names]
+print("First names in lowercase:", first_names)
